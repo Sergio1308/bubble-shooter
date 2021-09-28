a07bf7477a9d9ebb554fd00fbd341662886d1ede
Sergei Haiduk <sergi.haiduk@gmail.com>
2021-09-28 05:32:40 +0300

    removed unnecessary comments

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,20 +1,15 @@
 import java.awt.*;
 
 public class Bullet {
+    
     // Fields
     private double x;
     private double y;
-    //private double bulletdx;
-    //private double bulletdy;
 
     private double distX;
     private double distY;
-    //private double dist;
     private int r;
 
-//    private double dx;
-//    private double dy;
-//    private double rad;
     private double speed;
 
     private Color color;
@@ -24,18 +19,10 @@ public class Bullet {
         this.x = GamePanel.player.getX();
         this.y = GamePanel.player.getY();
         r = 2;
-
-//        rad = Math.toRadians(angle);
         speed = 20;
-//        dx = Math.cos(rad) * speed;
-//        dy = Math.sin(rad) * speed;
 
-        distX = GamePanel.mouseX - x; // разница по х от мышки до пули
+        distX = GamePanel.mouseX - x;  // subtracting the distance from the crosshair to the bullet
         distY = y - GamePanel.mouseY;
-        //dist = Math.sqrt(distX * distX + distY * distY);
-
-        //bulletdx = distX/dist * speed;
-        //bulletdy = distY/dist * speed;
 
         color = Color.WHITE;
     }
@@ -53,17 +40,8 @@ public class Bullet {
     }
 
     public boolean update() {
-//        y -= speed;
-       // y += Math.sin(x) * 4 + bulletdy;
-       // x += Math.sin(y) * 4 + bulletdx;
-
-        //y += bulletdy;
-        //x += bulletdx;
         y = y - speed * distY/(Math.sqrt(distX*distX + distY*distY));
         x = x + speed * distX/(Math.sqrt(distX*distX + distY*distY));
-
-//        x += dx;
-//        y += dy;
 
         if (x < -r || x > GamePanel.WIDTH + r ||
                 y < -r || y > GamePanel.HEIGHT + r) {
